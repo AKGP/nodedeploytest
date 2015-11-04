90ab311b758db5c02ec34bce89d864ce9db20ac4
Ajay <ajay720945@gmail.com>
2015-11-04 18:57:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var http = requre('http');
+var http = require('http');
 var app = express();
 var mongojs = require('mongojs');
 var bodyparser = require('body-parser');
